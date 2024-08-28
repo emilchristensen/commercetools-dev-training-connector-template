1738cca3bc9a3cc141d0b5de26bd196c7c93013a
Emil Rømer Christensen <emil.christensen@deptagency.com>
2024-08-28 18:00:18 +0200

    Update with demo code

--- customer-number-generator-app/src/controllers/customers.controller.ts
+++ customer-number-generator-app/src/controllers/customers.controller.ts
@@ -13,13 +13,21 @@ const create = async (resource: Resource) => {
   const updateActions: Array<UpdateAction> = [];
 
   // TODO: Create the UpdateAction Object to return it to the client
-
+  const updateAction: UpdateAction = {
+    action: "setCustomerNumber",
+    // generate a number between 1000 and 1899
+    customerNumber: String(Math.floor(1000 + Math.random() * 900)),
+  };
+  
+  updateActions.push(updateAction);
 
   return { statusCode: 201, actions: updateActions };
 };
 
 // Controller for update actions
-// const update = (resource: Resource) => {};
+// const update = (resource: Resource) => {
+
+// };
 
 /**
  * Handle the order controller according to the action
